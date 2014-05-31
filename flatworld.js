a3b2ba54ff0dd36a0053fc23971ea439b623f7f0
weirder <weirderweirdness@gmail.com>
2014-05-31 06:52:35 +0000

    Create flatworld.js

---
+++ flatworld.js
@@ -0,0 +1,84 @@
+function newLevel(){
+
+for(var i = 0; i < 256; i++){
+
+for(var p = 0; p < 256; p++){
+
+Level.setTile(i, 115, p, 0);
+Level.setTile(i, 114, p, 0);
+Level.setTile(i, 113, p, 0);
+Level.setTile(i, 112, p, 0);
+Level.setTile(i, 111, p, 0);
+Level.setTile(i, 110, p, 0);
+Level.setTile(i, 109, p, 0);
+Level.setTile(i, 108, p, 0);
+Level.setTile(i, 107, p, 0);
+Level.setTile(i, 106, p, 0);
+Level.setTile(i, 105, p, 0);
+Level.setTile(i, 104, p, 0);
+Level.setTile(i, 103, p, 0);
+Level.setTile(i, 102, p, 0);
+Level.setTile(i, 101, p, 0);
+Level.setTile(i, 100, p, 0);
+Level.setTile(i, 99, p, 0);
+Level.setTile(i, 98, p, 0);
+Level.setTile(i, 97, p, 0);
+Level.setTile(i, 96, p, 0);
+Level.setTile(i, 95, p, 0);
+Level.setTile(i, 94, p, 0);
+Level.setTile(i, 93, p, 0);
+Level.setTile(i, 92, p, 0);
+Level.setTile(i, 91, p, 0);
+Level.setTile(i, 90, p, 0);
+Level.setTile(i, 89, p, 0);
+Level.setTile(i, 88, p, 0);
+Level.setTile(i, 87, p, 0);
+Level.setTile(i, 86, p, 0);
+
+for(var f = 0; f < 2; f++){
+
+Level.setTile(i, 85, p, 0);
+Level.setTile(i, 84, p, 0);
+Level.setTile(i, 83, p, 0);
+Level.setTile(i, 82, p, 0);
+Level.setTile(i, 81, p, 0);
+Level.setTile(i, 80, p, 0);
+Level.setTile(i, 79, p, 0);
+Level.setTile(i, 78, p, 0);
+Level.setTile(i, 77, p, 0);
+Level.setTile(i, 76, p, 0);
+Level.setTile(i, 75, p, 0);
+Level.setTile(i, 74, p, 0);
+Level.setTile(i, 73, p, 0);
+Level.setTile(i, 72, p, 0);
+Level.setTile(i, 71, p, 0);
+Level.setTile(i, 70, p, 0);
+Level.setTile(i, 69, p, 0);
+Level.setTile(i, 68, p, 0);
+Level.setTile(i, 67, p, 0);
+Level.setTile(i, 66, p, 0);
+Level.setTile(i, 65, p, 2);
+
+}
+
+/*Level.setTile(i, 64, p, 0);
+Level.setTile(i, 63, p, 0);
+Level.setTile(i, 62, p, 0);
+Level.setTile(i, 61, p, 0);
+Level.setTile(i, 60, p, 2);
+Level.setTile(i, 59, p, 0);
+Level.setTile(i, 58, p, 0);
+Level.setTile(i, 57, p, 0);
+Level.setTile(i, 56, p, 0);
+Level.setTile(i, 55, p, 0);
+Level.setTile(i, 54, p, 0);
+Level.setTile(i, 53, p, 0);
+Level.setTile(i, 52, p, 0);
+Level.setTile(i, 51, p, 0);
+Level.setTile(i, 50, p, 2);*/
+
+}
+
+}
+
+}
